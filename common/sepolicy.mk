7b6222d3229a38e1a4f2fca59601929e41fdbbae
Kry9toN <dhimasbagusprayoga@gmail.com>
2019-06-13 13:27:21 +0000

    Adabt to KomodOS Rom

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -4,10 +4,10 @@
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/beast/sepolicy/common/public
+    device/komodo/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/beast/sepolicy/common/private
+    device/komodo/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/beast/sepolicy/common/vendor
+    device/komodo/sepolicy/common/vendor
